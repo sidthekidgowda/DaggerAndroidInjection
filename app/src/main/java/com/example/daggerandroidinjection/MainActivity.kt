5648ffbc52111ab8a9bf989940233ec1dbabb373
Sid <sidgowda.9@gmail.com>
2020-01-31 08:50:12 -0500

    removed activity submodule

--- app/src/main/java/com/example/daggerandroidinjection/MainActivity.kt
+++ app/src/main/java/com/example/daggerandroidinjection/MainActivity.kt
@@ -1,22 +1,17 @@
 package com.example.daggerandroidinjection
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
 import javax.inject.Inject
-import javax.inject.Named
 
 class MainActivity : AppCompatActivity(), HasAndroidInjector {
 
     @Inject
     lateinit var androidInjector: DispatchingAndroidInjector<Any>
-
-    @Inject
-    lateinit @field:Named("ActivityLog") var activityLog: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +23,6 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
             .replace(R.id.activity_container, MainFragment())
             .addToBackStack(null)
             .commit()
-
-        Log.d("TAG", activityLog)
     }
 
     override fun androidInjector(): AndroidInjector<Any> {
--- app/src/main/java/di/activity/MainActivitySubcomponent.kt
+++ app/src/main/java/di/activity/MainActivitySubcomponent.kt
@@ -5,7 +5,7 @@ import dagger.Subcomponent
 import dagger.android.AndroidInjector
 
 
-@Subcomponent(modules = [MainActivitySubModule::class])
+@Subcomponent
 interface MainActivitySubcomponent : AndroidInjector<MainActivity> {
 
     @Subcomponent.Factory
